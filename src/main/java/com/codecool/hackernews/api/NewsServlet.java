6a03e20b7b57d717b7cfb5e57b9fe2d68a42e45e
siposm17 <siposm17@gmail.com>
2021-06-02 22:40:33 +0200

    tasks done

---
+++ src/main/java/com/codecool/hackernews/api/NewsServlet.java
@@ -0,0 +1,2 @@
+package com.codecool.hackernews.api;public class NewsServlet {
+}
---
+++ src/main/java/com/codecool/hackernews/api/TopNewsServlet.java
@@ -0,0 +1,42 @@
+package api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@WebServlet(name="topNewsServlet", urlPatterns = {"http://localhost:8080/api/top"}, loadOnStartup = 4)
+public class TopNewsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        URL url = new URL("https://api.hnpwa.com/v0");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        PrintWriter out = resp.getWriter();
+
+        out.println(content.toString());
+        out.flush();
+
+    }
+}
